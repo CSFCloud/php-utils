8ca850f0be7990d5a155067ec1dd3907b6627ddf
Bence Skorka <skorka.bence@gmail.com>
2018-04-30 18:33:58 +0200

    temp dir creation

--- src/TempFiles/TempManager.php
+++ src/TempFiles/TempManager.php
@@ -15,12 +15,19 @@ class TempManager {
         if (!preg_match('/^[a-z0-9\_\-]+$/i', $context)) {
             throw new Exception("The temp context must be an alphanumeric string");
         }
-        if (!is_writable(self::TEMP_DIR)) {
-            throw new Exception("The temp directory must be writeable (" . self::TEMP_DIR . ")");
+        if (!file_exists(self::TEMP_DIR)) {
+            mkdir(self::TEMP_DIR, 0777, true);
+        }
+        if (is_writable(self::TEMP_DIR)) {
+            $this->dir = self::TEMP_DIR . "/" . $context;
+        } else {
+            $this->dir = sys_get_temp_dir() . "/csf/" . $context;
         }
         $this->dir = self::TEMP_DIR . "/" . $context;
         if (!file_exists($this->dir)) {
-            mkdir($this->dir);
+            if (!mkdir($this->dir, 0777, true)) {
+                throw new Exception("Creating new directory failed (" . $this->dir . ")");
+            }
         }
         if (!is_writable($this->dir)) {
             throw new Exception("The temp directory must be writeable (" . $this->dir . ")");
